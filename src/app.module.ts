ba74c6c39f09fbe35feb8f23b58eda4f82eb8572
Caio Emidio <caio.emidio@gmail.com>
2025-01-19 23:22:20 +0000

    fix modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { ImgbbModule } from './imgbb/imgbb.module';
 
 @Module({
   imports: [
     ImgbbModule.forRoot(process.env.IMGBB_API_KEY),
   ],
-  controllers: [AppController],
+  controllers: [],
 })
 export class AppModule {}
