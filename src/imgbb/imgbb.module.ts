22288b36635445f3a9a458bf4a1c97282e423250
Caio Emidio <caio.emidio@gmail.com>
2025-01-23 22:22:35 +0000

    forRootAsync

--- src/imgbb/imgbb.module.ts
+++ src/imgbb/imgbb.module.ts
@@ -1,14 +1,19 @@
 // src/imgbb/imgbb.module.ts
-import { Module, DynamicModule } from '@nestjs/common';
+import { Module, DynamicModule, ModuleMetadata, Provider } from '@nestjs/common';
 import { ImgbbService } from './imgbb.service';
 import { ImgbbController } from './imgbb.controller';
 import { HttpModule } from '@nestjs/axios';
+
+export interface ImgbbModuleAsyncOptions extends Pick<ModuleMetadata, 'imports'> {
+    useFactory: (...args: any[]) => Promise<string> | string;
+    inject?: any[];
+}
 
 @Module({
     imports: [HttpModule],
     providers: [ImgbbService],
     exports: [ImgbbService], // Certifique-se de exportar o serviço aqui
-  })
+})
 export class ImgbbModule {
     static forRoot(apiKey: string): DynamicModule {
         return {
@@ -24,5 +29,29 @@ export class ImgbbModule {
             ],
             exports: [ImgbbService],
         };
+    }
+
+    static forRootAsync(options: ImgbbModuleAsyncOptions): DynamicModule {
+        const asyncProviders = this.createAsyncProviders(options);
+        return {
+            module: ImgbbModule,
+            imports: options.imports || [],
+            controllers: [ImgbbController],
+            providers: [
+                ...asyncProviders,
+                ImgbbService,
+            ],
+            exports: [ImgbbService],
+        };
+    }
+
+    private static createAsyncProviders(options: ImgbbModuleAsyncOptions): Provider[] {
+        return [
+            {
+                provide: 'IMGBB_API_KEY',
+                useFactory: options.useFactory,
+                inject: options.inject || [],
+            },
+        ];
     }
 }
